6babc85f0bc7d012c4c65dcb370a20914a6d14bd
Richard Flynn <rjzflynn@gmail.com>
2019-04-07 17:54:52 +0100

    git formatting...

--- src/main/java/typicode_tests/smokeTests/FetchSmokeTests.java
+++ src/main/java/typicode_tests/smokeTests/FetchSmokeTests.java
@@ -11,46 +11,46 @@ public class FetchSmokeTests {
 	
 		
 	@Test
-	 public void fetchListTest() {
-		 given().
-		 	baseUri(URIUtils.getBaseUri()).
-		    pathParam("resource", "posts").
-		    pathParam("resource", "comments").
-		    pathParam("resource", "albums").
-		    pathParam("resource", "photos").
-		    pathParam("resource", "todos").
-		    pathParam("resource", "users").
-		 when().
-		 	get("/{resource}").
-		 then().
-	        assertThat().
-	        statusCode(200).
-	    and().
-	        contentType(ContentType.JSON).
+	public void fetchListTest() {
+		given().
+			baseUri(URIUtils.getBaseUri()).
+			pathParam("resource", "posts").
+			pathParam("resource", "comments").
+			pathParam("resource", "albums").
+			pathParam("resource", "photos").
+			pathParam("resource", "todos").
+			pathParam("resource", "users").
+		when().
+			get("/{resource}")
+		.then()
+			.assertThat().
+			statusCode(200).
 		and().
-			body("size()", greaterThan(5));
-	 }
-	
+			contentType(ContentType.JSON).
+		and()
+			.body("size()", greaterThan(5));
+	}
+
 	@Test
-	 public void fetchSingleTest() {
-		 given().
-		 	baseUri(URIUtils.getBaseUri()).
-		 	pathParam("resource", "posts").
-		    pathParam("resource", "comments").
-		    pathParam("resource", "albums").
-		    pathParam("resource", "photos").
-		    pathParam("resource", "todos").
-		    pathParam("resource", "users").
-		 when().
-		 	get("/{resource}/1").
-		 then().
-	        assertThat().
-	        statusCode(200).
-	    and().
-	        contentType(ContentType.JSON).
-		and().
-			body("isEmpty()", is(false));	
-	 }
+	public void fetchSingleTest() {
+		given().
+			baseUri(URIUtils.getBaseUri()).
+			pathParam("resource", "posts").
+			pathParam("resource", "comments").
+			pathParam("resource", "albums").
+			pathParam("resource", "photos").
+			pathParam("resource", "todos").
+			pathParam("resource", "users").
+		when().
+			get("/{resource}/1")
+		.then().
+			assertThat().
+			statusCode(200).
+		and()
+			.contentType(ContentType.JSON)
+		.and()
+			.body("isEmpty()", is(false));
+	}
 	
 	
 
