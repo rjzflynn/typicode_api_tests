d1ad66c3949c72b10433f857f273efd30eaaf81a
Richard Flynn <rjzflynn@gmail.com>
2019-04-15 16:15:26 +0100

    add scenario test: createNewPost_addCommentsToPost_fetchPost_assertOnTheCommentAndPost_test

--- src/main/java/typicode_tests/scenariosTests/ScenariosTests.java
+++ src/main/java/typicode_tests/scenariosTests/ScenariosTests.java
@@ -1,8 +1,73 @@
 package typicode_tests.scenariosTests;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import io.restassured.http.ContentType;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import utils.CommentUtils;
+import utils.PostsUtils;
+import utils.URIUtils;
+
 public class ScenariosTests {
 	
 	// test create new Post, then create comment on that post, then fetch post, assert on the post
 
-
+	@Test
+	public void createNewPost_addCommentsToPost_fetchPost_assertOnTheCommentAndPost_test() {
+		
+		int postId =
+		given().
+			baseUri(URIUtils.getBaseUri()).
+			contentType("application/json").
+			body(PostsUtils.newPost()).
+		when().
+			post("/posts").
+		then().
+			assertThat().
+			statusCode(201).
+		extract().
+	        path("id");
+		
+		
+		Map<String,String> comment = CommentUtils.newComment(postId);
+		int commentId =
+				
+		given().
+			baseUri(URIUtils.getBaseUri()).
+			contentType("application/json").
+			body(comment).
+		when().
+			post("/comments").
+			then().
+			assertThat().
+			statusCode(201).
+		and().
+			body(containsString(comment.get("body"))).
+		and().
+			body("postId", equalTo(String.valueOf(postId))).   //TODO: check returned types, String / int
+		extract().
+	        path("id");
+		
+		
+		//currenly wont work as the post is mocked and not really  created ...
+		given().
+	 		baseUri(URIUtils.getBaseUri()).
+		when().
+		 	get("/comments/" + String.valueOf(commentId)).
+		then().
+	        assertThat().
+	        statusCode(200).
+	    and().
+	    	body("id", equalTo(String.valueOf(commentId))).
+	    and().
+	    	body("postId", equalTo(String.valueOf(postId)));
+		
+		//TODO: more assertations on comment content ect.
+		
+	}
 }
--- src/main/java/utils/CommentUtils.java
+++ src/main/java/utils/CommentUtils.java
@@ -6,6 +6,11 @@ import java.util.Map;
 public class CommentUtils {
 	
 	private static final String NEW_COMMENT_POST_ID = "1";
+	
+	public static String getNewCommentPostId() {
+		return NEW_COMMENT_POST_ID;
+	}
+
 
 	public static Map<String, String> newComment() {
 		Map<String,String> comment =  new HashMap<String,String>();
@@ -22,8 +27,14 @@ public class CommentUtils {
 		return comment;
 	}
 	
-	public static String getNewCommentPostId() {
-		return NEW_COMMENT_POST_ID;
+	
+	public static Map<String, String> newComment(int postId) {
+		Map<String,String> comment =  new HashMap<String,String>();
+		comment.put("postId", String.valueOf(postId));
+		comment.put("name", "John Doe");
+		comment.put("email", "jd@gmail.com");
+		comment.put("body", "foo bar bar foo");
+		return comment;
 	}
 
 }
